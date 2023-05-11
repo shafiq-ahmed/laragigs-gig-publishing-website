e34973975a8f5917fddc46461080d01e1448274b
Shafiq Ahmed <shafiqahmd3@gmail.com>
2023-05-11 17:51:47 +0600

    ScopeFilter added

--- resources/views/components/listing-tag.blade.php
+++ resources/views/components/listing-tag.blade.php
@@ -1,13 +1,14 @@
 @props(['tagsCsv'])
 @php
-    $tags=explode(',',$tagsCsv);    
+    $tags=explode(',',$tagsCsv); 
+       
 @endphp
 <ul class="flex">
     @foreach($tags as $tag)
     <li
         class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
     >
-        <a href="#">{{$tag}}</a>
+        <a href="{{url()->current()}}?tag={{$tag}}">{{$tag}}</a>
     </li>
     @endforeach
 </ul>
